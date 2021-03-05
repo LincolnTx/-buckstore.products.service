3bade14c605ee1e9086ea96349201f669287d1bf
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-03-05 17:12:10 -0300

    feat: creating a common validation to share generic validations like ids use a common validation on AddProductRateValidations.cs

--- src/buckstore.products.service.application/Validations/AddProductRateValidations.cs
+++ src/buckstore.products.service.application/Validations/AddProductRateValidations.cs
@@ -1,13 +1,58 @@
-﻿using buckstore.products.service.application.Commands;
-using FluentValidation;
+﻿using FluentValidation;
+using buckstore.products.service.application.Commands;
 
 namespace buckstore.products.service.application.Validations
 {
     public class AddProductRateValidations : AbstractValidator<AddProductRateCommand>
     {
+        
         public AddProductRateValidations()
         {
-            // adicionar validações
+           ValidateProductCode();
+           ValidateRatePoints();
+           ValidateComment();
+           ValidateUserId();
         }
+
+        private void ValidateProductCode()
+        {
+            RuleFor(rate => rate.ProductCode)
+                .NotEmpty()
+                .WithMessage("Código do produto deve ser informado para realizar essa alteração")
+                .WithErrorCode("001")
+                .Must(candidate => CommonValidations.GuidValidator(candidate.ToString()))
+                .WithMessage("O código deve ser um uuid válido!")
+                .WithErrorCode("002");
+        }
+
+        private void ValidateRatePoints()
+        {
+            RuleFor(rate => rate.RatePoints)
+                .NotEmpty()
+                .WithMessage("Para adicionar uma avaliação o puntuação é obrigatória")
+                .WithErrorCode("003")
+                .ExclusiveBetween(1, 5)
+                .WithMessage("O valor da nota deve ser algo entre 1 e 5")
+                .WithErrorCode("004");
+        }
+
+        private void ValidateComment()
+        {
+            RuleFor(rate => rate.Comment)
+                .MaximumLength(300)
+                .WithMessage("O seu comentário ultrapassou o limete de caracteres, o máximo permitido é 300")
+                .WithErrorCode("005");
+        }
+
+        private void ValidateUserId()
+        {
+            RuleFor(rate => rate.UserId)
+                .NotEmpty()
+                .WithMessage("Por favor indique o id do usuário para realizar o cadastro")
+                .WithErrorCode("006")
+                .Must(candidate => CommonValidations.GuidValidator(candidate.ToString()));
+        }
+
+        
     }
 }
---
+++ src/buckstore.products.service.application/Validations/CommonValidations.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace buckstore.products.service.application.Validations
+{
+    public static class CommonValidations
+    {
+        private static readonly Regex IsGuid = new Regex(
+            @"^({){0,1}[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(}){0,1}$",
+            RegexOptions.Compiled);
+        
+        public static bool GuidValidator(string guidCheck)
+        {
+            return IsGuid.IsMatch(guidCheck);
+        }
+    }
+}
