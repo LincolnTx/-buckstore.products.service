968a9784331a6f157b52139886af2e5e5ff952e8
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-06-17 10:43:58 -0300

    feat: basic producer and consumer implementations

---
+++ src/buckstore.products.service.application/Adapters/MessageBroker/IMessageProducer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using buckstore.products.service.application.IntegrationEvents;
+
+namespace buckstore.products.service.application.Adapters.MessageBroker
+{
+    public interface IMessageProducer<in TEvent> where TEvent : IntegrationEvent
+    {
+        Task Produce(TEvent message);
+    }
+}
