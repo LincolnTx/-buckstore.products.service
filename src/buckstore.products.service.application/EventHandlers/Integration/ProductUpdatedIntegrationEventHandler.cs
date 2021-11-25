4a7226e7083c4455af1b280f3c71806628220bb8
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-11-25 13:10:45 -0300

    feat: create event handler to product update kafka consumer

---
+++ src/buckstore.products.service.application/EventHandlers/Integration/ProductUpdatedIntegrationEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using buckstore.products.service.application.IntegrationEvents;
+using buckstore.products.service.domain.Aggregates.ProductAggregate;
+using buckstore.products.service.domain.Exceptions;
+using buckstore.products.service.domain.SeedWork;
+using MediatR;
+
+namespace buckstore.products.service.application.EventHandlers.Integration
+{
+    public class ProductUpdatedIntegrationEventHandler : EventHandler<ProductUpdatedIntegrationEvent>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _uow;
+        private readonly IMediator _bus;
+        private readonly IMapper _mapper;
+
+        public ProductUpdatedIntegrationEventHandler(IProductRepository productRepository, IUnitOfWork uow, IMediator bus, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _uow = uow;
+            _bus = bus;
+            _mapper = mapper;
+        }
+
+        public override async Task Handle(ProductUpdatedIntegrationEvent notification, CancellationToken cancellationToken)
+        {
+            var productFound = await _productRepository.FindById(notification.Id);
+
+            if (productFound == null)
+            {
+                await _bus.Publish(new ExceptionNotification("010",
+                        "Produto para ser atualizado, não foi encontrado"),
+                    cancellationToken);
+
+                return;
+            }
+
+            productFound.UpdateProduct(notification.Name, notification.Description, notification.Price,
+                notification.Quantity, notification.CategoryId);
+
+            if (!await _uow.Commit())
+            {
+                await _bus.Publish(new ExceptionNotification("010",
+                        "Erro ao salvar atualização no banco"),
+                    cancellationToken);
+            }
+        }
+    }
+}
